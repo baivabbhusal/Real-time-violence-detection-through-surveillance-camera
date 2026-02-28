f2cf9b91059c64bca178261ad88ccfa2738310c2
Baivab Bhusal <baivabbhusal229@gmail.com>
2026-02-28 18:09:17 +0545

    custom button component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Button from './components/Button'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
     <div>
       <h1>Real time violence detection system</h1>
+      <p>Designed By Baivab Bhusal</p>
+      <Button buttonName='Sign-up'/>
     </div>
     </>
   )
---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({buttonName,className}) => {
+  return (
+    <button className='bg-amber-400'>{buttonName}</button>
+  )
+}
+
+export default Button
